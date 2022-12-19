eb0671ee891b1930b7eb4135143e26d96c2d6a6e
Rinad <921837@gmail.com>
2022-12-19 22:33:40 +0300

    1

---
+++ sem1.java
@@ -0,0 +1,96 @@
+
+/**
+ * sem1
+ */
+
+// Задача 1. Вычислить n-ое треугольного число
+// (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+/* 
+
+public class sem1 {
+
+    public static void main(String[] args) {
+        int fact = 1;
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Факторил " + n + " = " + fact);
+
+    }
+
+}
+
+*/
+
+
+// Вывести все простые числа от 1 до 1000.
+
+/** 
+public class sem1 {
+
+    public static void main(String[] args) {
+
+        int i=0;
+
+        while (i !=1000) {
+            i++;
+            System.out.print(i+" ");
+
+        }
+
+        
+
+    }
+
+}
+
+*/
+
+// Реализовать простой калькулятор.
+
+/** 
+
+import java.io.Reader;
+import java.util.Scanner;
+public class sem1 {
+
+    public static void main(String[] args) {
+
+        double num1;
+        double num2;
+        double ans;
+        char op;
+
+        Scanner reader = new Scanner (System.in);
+        System.out.print("Введите первое число: ");
+        num1=reader.nextDouble();
+        System.out.print("Введите второе число: ");
+        num2=reader.nextDouble();
+        System.out.println("Введите оператор (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        switch (op) {
+            case '+': ans = num1+num2;
+                break;
+            case '-': ans = num1-num2;
+                break;
+            case '*': ans = num1*num2;
+                break;
+            case '/': ans = num1/num2;
+                break;
+            default: System.out.println("Ошибка! Введите правильный оператор!");
+                return;
+        }
+        System.out.println("Результат: ");
+        System.out.println(num1 + " "+ op + " "+ num2+"=" + ans);
+
+
+        
+
+    }
+
+}
+*/
