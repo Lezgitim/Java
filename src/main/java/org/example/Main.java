707025cf748d5000b9ff52bb5f216a0b4e041ba0
Rinad <921837@gmail.com>
2022-12-30 15:07:19 +0300

    15

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        linked();
-//         ochered();
-calc();
+//        ochered();
+            calc();
     }
 
 //    1. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
